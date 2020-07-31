0c123a0c7331c5315d7abefade90edf37b3a97ad
Arnav Singh <arnavbhaiya@gmail.com>
2020-07-31 21:49:27 +0200

    First commit

---
+++ 3SumClosest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+    }
+};
+
+
+
+int main(){
+
+vector<int> vec1={-1,2,1,-4};
+int target=1;
+
+threeSumClosest(nums,target);
+
+
+    return 0;
+}
